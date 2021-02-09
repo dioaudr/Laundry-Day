48c9b8e9f0ea7986c5eab93b2e87a9e75d378073
Dio Audia <76801729+dioaudr@users.noreply.github.com>
2021-02-09 10:12:54 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+// insert your code here
+
+//PSUDOCODE
+/** FOR cuci FROM 1 TO 20 INCREMENT BY 1
+  DISPLAY 'Pakaian Sedang Dicuci' PLUS cuci
+END FOR
+ */
+for(var cuci = 1; cuci <= 20; cuci++){
+    console.log("Pakaian sedang dicuci " + cuci)
+}
